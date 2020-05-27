37419b5084f3e7b930e24b2a6f33a0bb7fb91243
RockeR26 <65093060+RockeR26@users.noreply.github.com>
2020-05-27 18:09:18 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
-function btn(){
-    document.querySelector("h2").innerHTML="Player 1 tap the dice";
-}
+var rand2 = Math.floor(Math.random()*6+1);
 
 var rand1 = Math.floor(Math.random()*6+1);
 
+document.querySelector(".btn").addEventListener("click",btn);
+
+
+function btn(){
+        document.querySelector("h2").innerHTML="Player 1 tap the dice";
+        document.querySelector(".img1").addEventListener("click",ran1); 
+}
 
 function ran1(){
-    document.querySelector(".img1").setAttribute("src","images/dice"+rand1+".png");
-    document.querySelector("h2").innerHTML="Player 2 tap the dice";
+        document.querySelector(".img1").setAttribute("src","images/dice"+rand1+".png");
+        document.querySelector("h2").innerHTML="Player 2 tap the dice";
+        document.querySelector(".img2").addEventListener("click",ran2);
 }
-
-var rand2 = Math.floor(Math.random()*6+1);
 
 function ran2(){
-    document.querySelector(".img2").setAttribute("src","images/dice"+rand2+".png");
-    if(rand1>rand2){
-        document.querySelector("h2").innerHTML="🚩 Player 1 Wins 🎉";
-    }
-    else if(rand1===rand2){
-        document.querySelector("h2").innerHTML="Its a Tie 🤣🤣";
-    }
-    else{
-        document.querySelector("h2").innerHTML=" 🚩 Player 2 Wins 🎉 ";
-    }
-    document.querySelector(".para").innerHTML="Tap the Reset button to start again"
+        document.querySelector(".img2").setAttribute("src","images/dice"+rand2+".png");
+        if(rand1>rand2){
+            document.querySelector("h2").innerHTML="🚩 Player 1 Wins 🎉";
+        }
+        else if(rand1===rand2){
+            document.querySelector("h2").innerHTML="Its a Tie 🤣🤣";
+        }
+        else{
+            document.querySelector("h2").innerHTML=" 🚩 Player 2 Wins 🎉 ";
+        }
+        document.querySelector(".para").innerHTML="Tap the Reset button to start again"; 
 }
+
+
 
 
